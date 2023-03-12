ffa79ad74a43b8ce58ce2c2335a39536012eafaf
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-03-12 20:42:40 +0100

    all part of user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ,],
 })
 export class AppModule {}
