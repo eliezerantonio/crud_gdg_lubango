00c26734ec18675ae99a997d619671d9e3af8898
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-03-12 21:04:46 +0100

    feat: added  DTO to createUser

--- src/users/dto/create-user.dto/create-user.dto.ts
+++ src/users/dto/create-user.dto/create-user.dto.ts
@@ -1,1 +1,12 @@
-export class CreateUserDto {}
+import { IsNumber, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  readonly name: string;
+
+  @IsNumber()
+  readonly age: Number;
+
+  @IsString()
+  readonly phone: string;
+}
