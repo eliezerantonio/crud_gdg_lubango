0333eea1c9a0fc12b0b112a860f398638675e65c
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-03-13 04:24:51 +0100

    feat: Add users_migrations

--- src/database.providers.ts
+++ src/database.providers.ts
@@ -1,4 +1,5 @@
 import { DataSource } from 'typeorm';
+import { CreateUsersTable1678676253058 } from './migrations/1678676253058-CreateUsersTable';
 
 export const databaseProviders = [
   {
@@ -11,12 +12,25 @@ export const databaseProviders = [
         username: 'postgres',
         password: 'docker',
         database: 'gdgdb',
-          entities: [__dirname + '/../**/*.entity.js'],
-        
-        synchronize: true,
+        entities: [__dirname + '/../**/*.entity.js'],
+
+        synchronize: false,
       });
 
       return dataSource.initialize();
     },
   },
 ];
+
+export const dataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'docker',
+  database: 'gdgdb',
+  entities: [__dirname + '/../**/*.entity.js'],
+
+  synchronize: false,
+  migrations: [CreateUsersTable1678676253058],
+});
---
+++ src/migrations/1678676253058-CreateUsersTable.ts
@@ -0,0 +1,30 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUsersTable1678676253058 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          { name: 'id', type: 'uuid', isPrimary: true },
+          { name: 'name', type: 'varchar' },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          { name: 'age', type: 'integer' },
+          { name: 'id', type: 'varchar' },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
